c215b1d585c21cd2431e6855fbb9360b9836093f
Jabari Bell <jabari.bell@pxlflu.net>
2015-05-14 17:05:38 -0400

    animating

--- GIFMonster/ViewController.m
+++ GIFMonster/ViewController.m
@@ -18,12 +18,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIView *container = [[UIView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:container];
 
     FLAnimatedImage *image = [FLAnimatedImage animatedImageWithGIFData:[NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://raphaelschaad.com/static/nyan.gif"]]];
     FLAnimatedImageView *imageView = [[FLAnimatedImageView alloc] init];
     imageView.animatedImage = image;
-    imageView.frame = CGRectMake(0.0, 0.0, 100.0, 100.0);
-    [self.view addSubview:imageView];
+    imageView.frame = CGRectMake(0.0, 0.0, 300.0, 300.0);
+    [container addSubview:imageView];
+
+    UILabel *myLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 70, 240, 20)];
+    myLabel.text = @"BAWS";
+    myLabel.font = [UIFont boldSystemFontOfSize:20];
+    myLabel.textColor = [UIColor whiteColor];
+    [container addSubview:myLabel];
+
+
+
+    UIGraphicsBeginImageContext(CGSizeMake(300,300));
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    [container.layer renderInContext:context];
+    UIImage *screenShot = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    UIImageView *snapshot = [[UIImageView alloc] initWithFrame:CGRectMake(0, 300, 300, 300)];
+    snapshot.image = screenShot;
+    [self.view addSubview:snapshot];
+
 }
 
 - (void)didReceiveMemoryWarning {
